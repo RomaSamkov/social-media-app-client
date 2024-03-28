775eb572b0640d10d54a66cee9d0ef5a1e73622c
RomaSamkov <Romansamkov.rs@gmail.com>
2024-03-28 17:12:48 +0200

    add ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./scenes/homePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
