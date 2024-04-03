550177fbcd5187956dd71ef0690c4695168f6dad
RomaSamkov <Romansamkov.rs@gmail.com>
2024-04-03 13:33:15 +0300

    Update FriendListWidget.jsx

--- src/scenes/widgets/FriendListWidget.jsx
+++ src/scenes/widgets/FriendListWidget.jsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setFriends } from "state";
 
+// eslint-disable-next-line react/prop-types
 const FriendListWidget = ({ userId }) => {
   const dispatch = useDispatch();
   const { palette } = useTheme();
@@ -13,7 +14,7 @@ const FriendListWidget = ({ userId }) => {
 
   const getFriends = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${userId}/friends`,
+      `https://social-media-app-server-kkom.onrender.com/users/${userId}/friends`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
